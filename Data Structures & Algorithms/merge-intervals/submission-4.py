a783bce25f40061580db6326117a31ac241a372c
Angela Le <angelale9140@gmail.com>
2026-09-17 13:49:18 -0500

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Return array of non-overlapping intervals, any order
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_intervals = sorted(intervals)
+        nonoverlap = [sorted_intervals[0]]
+
+        for i in range(1, len(sorted_intervals)):
+            prev = nonoverlap[-1]
+            current = sorted_intervals[i]
+
+            if current[0] <= prev[1]:
+               prev[1] = max(prev[1], current[1])
+
+            else:
+                nonoverlap.append(current)
+
+        return nonoverlap
